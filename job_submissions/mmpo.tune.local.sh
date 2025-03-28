92e4ab29f451bd19686b997eac7d6bb3838ad59c
SaeedNajafi <snajafi@ualberta.ca>
2025-03-28 20:35:23 +0000

    save

--- job_submissions/mmpo.tune.local.sh
+++ job_submissions/mmpo.tune.local.sh
@@ -2,13 +2,16 @@
 
 date;pwd
 
+eval "$(conda shell.bash hook)"
+conda activate dpo-llm-env
+
 export MASTER_ADDR="$(hostname --fqdn)"
 export MASTER_PORT="$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1])')"
 export RDVZ_ID=$RANDOM
 echo "RDZV Endpoint $MASTER_ADDR:$MASTER_PORT"
 
-export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
-NUM_GPUs=8
+export CUDA_VISIBLE_DEVICES="4,5,6,7"
+NUM_GPUs=4
 
 # export TORCH_DISTRIBUTED_DEBUG=DETAIL
 # export NCCL_DEBUG=WARN
@@ -16,21 +19,21 @@ NUM_GPUs=8
 # export TORCH_CPP_LOG_LEVEL=INFO
 # export LOGLEVEL=INFO
 export NCCL_ASYNC_ERROR_HANDLING=1
-export WANDB_PROJECT="llama3-8b-full"
+export WANDB_PROJECT="llama3-1b-v4"
 export WANDB_MODE="offline"
 export ACCELERATE_LOG_LEVEL="info"
 
 NUM_PROCS=${NUM_GPUs}
 
-LOG_DIR="mmpo_training_logs"
+LOG_DIR="mmpo_1b_training_logs"
 # Make logging directories.
 mkdir -p "${LOG_DIR}"
 
 echo "Placing logs in: ${LOG_DIR}"
 echo "GPUs per node: ${NUM_GPUs}"
 
-lrs=(0.000001)
-betas=(0.1)
+lrs=(0.0001)
+betas=(0.01)
 mmpo_relu_epsilon=(0.5)
 reward_epsilons=(0.0)
 
@@ -46,7 +49,7 @@ do
             for b in ${!betas[@]};
             do
                 beta=${betas[$b]}
-                RUN_NAME="llama3-8b-offline-mmpo-beta-${beta}-lr-${lr}-reward_eps_${r_eps}-relu-epsilon-${r_relu_eps}-full_standard_log-without-relu"
+                RUN_NAME="llama3-1b-offline-mmpo-beta-${beta}-lr-${lr}-reward_eps_${r_eps}-relu-epsilon-${r_relu_eps}"
                 accelerate launch \
                     --config_file=recipes/accelerate_configs/deepspeed_zero2.yaml \
                     --num_machines 1 \
@@ -55,12 +58,12 @@ do
                     --main_process_port $MASTER_PORT \
                     --machine_rank 0 \
                     --rdzv_conf "rdzv_backend=c10d,rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT" \
-                    scripts/run_dpo.py recipes/llama-3-8b/mmpo/config_qlora.yaml \
+                    scripts/run_dpo.py recipes/llama-3.2-1b/mmpo/config_qlora.yaml \
                         --learning_rate=${lr} \
                         --beta=${beta} \
                         --mmpo_reward_epsilon=${r_eps} \
                         --mmpo_relu_epsilon=${r_relu_eps} \
-                        --output_dir=/work/saeed/narval/mmpo_8b_full_standard_log_without_relu/${RUN_NAME} \
+                        --output_dir=/work/saeed/narval/mmpo_1b_v4/${RUN_NAME} \
                         --run_name=${RUN_NAME} > ${LOG_DIR}/log_${RUN_NAME}.log 2>&1
             done
         done
