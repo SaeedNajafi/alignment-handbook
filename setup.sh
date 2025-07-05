928f7758d0b6dbbd96dd8e278a3b12fd6d9f7b63
Saeed Najafi <saeednjf@rorqual1.rorqual.calcul.quebec>
2025-07-05 12:32:06 -0400

    New runs on rorqual.

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ ENV_NAME=$1
 
 module --force purge
 
-conda create -n ${ENV_NAME} python=3.11
+conda create -n ${ENV_NAME} python=3.12
 
 eval "$(conda shell.bash hook)"
 
@@ -15,24 +15,24 @@ conda activate ${ENV_NAME}
 echo "Installing cxx-compiler and gxx and gcc."
 conda install -c conda-forge cxx-compiler gxx gcc -y
 
-echo "Installing git."
+# echo "Installing git."
 conda install -c anaconda git -y
 
-echo "Installing git-lfs."
+# echo "Installing git-lfs."
 conda install -c conda-forge git-lfs -y
 
-echo "Installing rust."
+# echo "Installing rust."
 conda install -c conda-forge rust -y
 
 echo "Installing cuda drivers."
-conda install -c nvidia/label/cuda-12.4.1 cuda cuda-nvcc cuda-toolkit -y
+conda install -c nvidia/label/cuda-12.6.3 cuda cuda-nvcc cuda-toolkit -y
 conda install -c conda-forge cudnn -y
 
 echo "Upgrade pip."
 pip3 install --upgrade pip
 
 echo "Install torch."
-pip3 install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 --index-url https://download.pytorch.org/whl/cu124
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
 
 export CUDA_HOME=$CONDA_PREFIX
 export NCCL_HOME=$CONDA_PREFIX
@@ -40,6 +40,6 @@ export LD_LIBRARY_PATH=$CONDA_PREFIX/lib
 
 pip3 install --no-cache-dir flash-attn --no-build-isolation
 
-pip3 install vllm llvmlite vllm-flash-attn
+# pip3 install vllm llvmlite vllm-flash-attn
 
 # pip3 install flashinfer -i https://flashinfer.ai/whl/cu121/torch2.4
